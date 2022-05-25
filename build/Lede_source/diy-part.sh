5654d81608c8086a8a28c4dbdbbc92c2a3293c3d
cangzair <101318187+cangzair@users.noreply.github.com>
2022-05-25 17:22:40 +0800

    Add files via upload

---
+++ build/Lede_source/diy-part.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# DIY扩展二合一了，在此处可以增加插件
+# 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
+# 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
+# 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
+# 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
+# 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
+
+wget https://raw.githubusercontent.com/cangzair/Public/main/x86/x86.sh
+bash x86.sh
+sed -i 's/02b79d5e2b07b5e64cd28f1fe84395ee11eef95fc49fd923a9ab93022b148be6/skip/g' feeds/packages/utils/containerd/Makefile
+# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间
+cat >${GITHUB_WORKSPACE}/Clear <<-EOF
+rm -rf config.buildinfo
+rm -rf feeds.buildinfo
+rm -rf openwrt-x86-64-generic-kernel.bin
+rm -rf openwrt-x86-64-generic.manifest
+rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
+rm -rf sha256sums
+rm -rf version.buildinfo
+EOF
